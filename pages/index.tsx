f6b29f27d488c4be80d36c8a67e47d2496a2ec45
Cash DeLeon <cashd@users.noreply.github.com>
2022-04-04 14:44:51 -0500

    fixed script

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,37 +3,52 @@ import React, { ReactElement } from "react";
 import PageView from "src/ui/app/PageView";
 import { ProposalsJson } from "@elementfi/elf-council-proposals";
 import { PROPOSALS_JSON_URL } from "src/elf-council-proposals";
-import { getRecentDelegators } from "src/ui/overview/getRecentDelegators";
+import {
+  getRecentDelegators,
+  WhitelistData,
+} from "src/ui/overview/getRecentDelegators";
 
 interface HomeProps {
   proposalsJson: ProposalsJson;
-  recentDelegators: string[];
+  whitelist: string[];
+  whitelistData: WhitelistData[];
+  blockNumbers: number[];
 }
 export default function Home({
   proposalsJson,
-  recentDelegators,
+  whitelist,
+  whitelistData,
+  blockNumbers,
 }: HomeProps): ReactElement {
-  console.log(recentDelegators);
+  console.log(whitelist);
+  console.log(whitelistData);
+
+  const min = Math.min(...blockNumbers);
+  const max = Math.max(...blockNumbers);
+  console.log("first block number: ", min);
+  console.log("Last block number: ", max);
+  console.log("total time since epoch (gov launch) ", max - min);
   return (
     <PageView childrenContainerClassName="flex justify-center">
       <OverviewPage
         proposalsJson={proposalsJson}
-        recentDelegators={recentDelegators}
+        recentDelegators={whitelist}
       />
     </PageView>
   );
 }
 
 export async function getStaticProps(): Promise<{
-  props: { proposalsJson: ProposalsJson; recentDelegators: string[] };
+  props: HomeProps;
   revalidate: number;
 }> {
-  const recentDelegators = await getRecentDelegators();
+  const { whitelist, whitelistData, blockNumbers } =
+    await getRecentDelegators();
   const res = await fetch(PROPOSALS_JSON_URL);
   const proposalsJson = await res.json();
 
   return {
-    props: { proposalsJson, recentDelegators },
+    props: { proposalsJson, whitelist, whitelistData, blockNumbers },
     revalidate: 5, // seconds,
   };
 }
--- src/ui/overview/getRecentDelegators.ts
+++ src/ui/overview/getRecentDelegators.ts
@@ -1,4 +1,3 @@
-import { BigNumber } from "ethers";
 import {
   lockingVaultContract as lockingVault,
   vestingContract as vestingVault,
@@ -7,9 +6,18 @@ import {
 const STARTING_BLOCK_NUMBER = 14496292;
 const MAX_WHITELIST = 2000;
 
+export interface WhitelistData {
+  address: string;
+  block: number;
+}
+
 // Fetches the number of unique delegators from both locking and vesting vaults
 // This function should be used within NextJs getStaticProps with a TTL to cache this result
-export async function getRecentDelegators(): Promise<string[]> {
+export async function getRecentDelegators(): Promise<{
+  whitelist: string[];
+  whitelistData: WhitelistData[];
+  blockNumbers: number[];
+}> {
   // Query for all events
   const lockingFilter = lockingVault.filters.VoteChange(null, null, null);
   const vestingFilter = vestingVault.filters.VoteChange(null, null, null);
@@ -23,23 +31,32 @@ export async function getRecentDelegators(): Promise<string[]> {
     STARTING_BLOCK_NUMBER,
   );
 
-  const delegators: Set<string> = new Set();
+  // const delegators: Set<string> = new Set();
+  const blockNumbers: Array<number> = [];
+  const whitelist: Set<string> = new Set();
+  const whitelistData: Array<WhitelistData> = [];
 
-  const sortedEvents = lockingEvents
-    .concat(vestingEvents)
-    .sort((eventA, eventB) => eventB.blockNumber - eventA.blockNumber);
+  const allEvents = lockingEvents.concat(vestingEvents);
+  const sortedEvents = allEvents.sort(
+    (eventA, eventB) => eventA.blockNumber - eventB.blockNumber,
+  );
 
   sortedEvents.forEach((event) => {
-    // VoteChange(to, from, amount)
     if (event.args) {
-      const from: string = event.args[1];
-      const value: BigNumber = event.args[2];
+      const from = event.args.from;
+      const value = event.args.amount;
 
-      if (value.gt(0) && delegators.size < MAX_WHITELIST) {
-        delegators.add(from);
+      if (value.gt(0) && whitelist.size < MAX_WHITELIST) {
+        whitelist.add(from);
+        whitelistData.push({ address: from, block: event.blockNumber });
+        blockNumbers.push(event.blockNumber);
       }
     }
   });
 
-  return Array.from(delegators);
+  return {
+    whitelist: Array.from(whitelist.values()),
+    whitelistData,
+    blockNumbers,
+  };
 }
